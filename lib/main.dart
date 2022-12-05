07b5ca011c9d744810320470497af536f8fef9d7
shmsacr <shmsacr@gmail.com>
2022-12-05 13:59:23 +0300

    feat: use change notifier provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
+
+final helloText = Provider((ref) => "HelloWord");
+final counterProvider = ChangeNotifierProvider((ref) => IncrementCounter());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,16 +14,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Material App Bar"),
-        ),
-        body: Center(
-          child: Container(
-            child: Text("Hello World"),
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.blueGrey),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch<IncrementCounter>(counterProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Material App Bar"),
+      ),
+      body: Center(child: Text(count.value.toString())),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          count.incCount();
+        },
+        child: Icon(Icons.add),
       ),
     );
+  }
+}
+
+class IncrementCounter extends ChangeNotifier {
+  int _value = 0;
+  int get value => _value;
+
+  incCount() {
+    _value++;
+    notifyListeners();
   }
 }
