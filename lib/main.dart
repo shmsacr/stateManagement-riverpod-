2b7f5782e1a3f77561fde3753ea108c00b631d36
shmsacr <shmsacr@gmail.com>
2022-12-05 13:22:49 +0300

    feat: initialize riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyFirstClass {
-  final String myValue = "First Class";
-}
-
-class MySecondClass {
-  final MyFirstClass firstClass;
-  MySecondClass(this.firstClass);
 }
 
 class MyApp extends StatelessWidget {
@@ -20,36 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Provider(
-      create: (context) => MyFirstClass(),
-      child: ProxyProvider<MyFirstClass, MySecondClass>(
-        update: (context, firstClass, secondClass) {
-          return MySecondClass(firstClass);
-        },
-        child: Scaffold(
-          appBar: AppBar(),
-          body: Builder(builder: (context) {
-            return Center(
-              child: Text(
-                Provider.of<MySecondClass>(context).firstClass.myValue,
-                style: TextStyle(fontSize: 20),
-              ),
-            );
-          }),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Material App Bar"),
+        ),
+        body: Center(
+          child: Container(
+            child: Text("Hello World"),
+          ),
         ),
       ),
     );
